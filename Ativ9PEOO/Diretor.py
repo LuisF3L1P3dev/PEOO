f5e6290e85e2b00c7606f95f1082ac82dc09b4b6
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2022-12-15 14:40:01 -0300

    fiz o sistema interno

--- Ativ9PEOO/Diretor.py
+++ Ativ9PEOO/Diretor.py
@@ -8,7 +8,12 @@ class Funcionario:
     def get_bonificacao(self):
         return self._salario * 0.10     
 
-class Gerente(Funcionario):
+class Autenticavel:
+
+    def autentica(self, senha):
+        pass
+
+class Gerente(Funcionario, Autenticavel):
 
     def __init__(self, nome, cpf, salario, senha, qtd_gerenciaveis):
         super().__init__(nome, cpf, salario)
@@ -26,7 +31,7 @@ class Gerente(Funcionario):
             print('Senha errada tente novamente')
             return False
 
-class Diretor(Funcionario):
+class Diretor(Funcionario, Autenticavel):
     def __init__(self, nome, cpf, salario, senha):
         super().__init__(nome, cpf, salario)
         self._senha = senha
@@ -34,14 +39,19 @@ class Diretor(Funcionario):
     def autentica(self, senha):
         if self._senha == senha:
             print(Gerente,"autencicado com sucesso")
+            return True
         else:
             print('Senha errada tente novamente')
-            
+            return False
+
+class Cliente(Autenticavel):
+    pass
+
 class SistemaInterno:
 
     def login(self, funcionario):
         if (hasattr(funcionario, 'autentica')):
-            funcionario.autentica(' ')
+            funcionario.autentica()
             # chama método autentica
             return True
         else:
@@ -49,17 +59,29 @@ class SistemaInterno:
             # imprime mensagem de ação inválida 
             return False
 
+class FuncionarioAutenticavel(Funcionario):
+
+    def autentica(self, senha):
+        pass
+
+
 
 
 if __name__ == '__main__':
     print()
     f1 = Funcionario("carlos",78481515, 2000)
     g1 = Gerente("marcos",13516885, 40000, '123', 10)
-    d1 = Diretor('Half',7484487478,50000,'321'
-    )
+    d1 = Diretor('Half',7484487478,50000,'321')
     system1 = SistemaInterno()
+    funAut = FuncionarioAutenticavel('Luis',8948794,1000)
     print("Bonificação funcionario:",f1.get_bonificacao())
     print("Bonificação Gerente:",g1.get_bonificacao())
-    g1.autentica('123')
+    #g1.autentica('123')
 
+    print(system1.login(f1))
     print(system1.login(g1))
+    print(system1.login(d1))
+    #no d1 ainda mostra que a senha está errada mas mostra como true, porvavelmente pq na senha do d1 está diferente a da classe login
+    print(funAut.autentica(g1))
+
+
