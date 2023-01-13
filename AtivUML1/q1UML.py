bf110f8cb8e45b669c77f56531d0c305022257e6
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2023-01-13 16:04:33 -0300

    q1 autenticacao

--- AtivUML1/q1UML.py
+++ AtivUML1/q1UML.py
@@ -26,7 +26,9 @@ class Postagem:
         return('id: {}\ntitulo: {}\ntexto: {}\ndataPublicacao: {}\n'.format(self.id, self.titulo, self.texto, self.dataPublicacao))
         
 class Blog:
-    
+
+    postagensPublicadas = []
+
     def __init__(self):
         self.postagens = []
 
@@ -40,14 +42,14 @@ class Blog:
             if i.id == postagem.id:
                 return postagem.id
     
-    def listarPostagensPublicadas(self):
-        postagensPublicadas = []
+    def listarPostagensPublicadas(self):     
         for i in self.postagens:
             if i.dataPublicacao < datetime.now():
-                postagensPublicadas.append(i)
+                self.postagensPublicadas.append(i)
             else:
                 print('data errada')
-            return postagensPublicadas
+        
+        return self.postagensPublicadas
     
     def listarTodasAsPostagens(self):
         return self.postagens
@@ -57,9 +59,16 @@ class Blog:
             if i.id == postagem.id:
                 self.postagens.remove(postagem)
 
+    def loginUsers(self):
+        login = str(input('login: '))
+        senha = str(input('senha: ')) 
+        if login == self.login() and senha == self.senha():
+        
+
+
 if __name__ == "__main__":
     user1 = Usuario(1, 'luis', 'luis', '1234')
-    post1 = Postagem(1, 'economia', 'oioiioi', date(2022, 5, 20))
+    post1 = Postagem(1, 'economia', 'oioiioi', datetime(2022, 5, 20))
     post2 = Postagem(2, 'saúde', 'caracois', datetime.now())
     post3 = Postagem(3, 'educação', 'abelhas', date(2022, 12, 31))
     blog = Blog()
@@ -93,24 +102,41 @@ if __name__ == "__main__":
     #Apagar todas as postagens
     blog.apagarPostagens(post1)
 
-    listUsers = [] #lista de usuarios autenticados
+    listUsers = [user1] #lista de usuarios autenticados
     verificacao = None
 
     while verificacao != 0:
-        verificacao = int(input('1-Criar usuario\n2-Usuario Cadastrado\n0-sair'))
+        verificacao = int(input('1-Criar usuario\n2-Usuario Cadastrado\n0-sair\n'))
         if verificacao == 1:
-         
+            
             id = input('id: ')
             nome = str(input('nome: '))
             login = str(input('login: '))
             senha =  str(input('senha: '))
 
-        user = Usuario(id, nome, login, senha)
-        listUsers.append(user)
+            user = Usuario(id, nome, login, senha)
+            listUsers.append(user)
+
+        #caso o usuario já estivar cadastrado, vai poder fazer os procedimentos.
 
         if verificacao == 2:
-            verificacao = int(input('3-Adiconar Postagem:\n4-Publicar Postagens:\n5-Listar Postagens Publicadas:\n6-Listar Todas as Postagens\n7-Apagar Postagens'))
-            
+            print('Usuarios:')
+            for i in listUsers:
+                print(' ',i.id,'-',i.nome,'\n')
+
+            login = str(input('login: '))
+            senha = str(input('senha: '))
+
+            if login == self.login() and senha == self.senha():
+
+                
+            verificacao = int(input('3-Adiconar Postagem:\n4-Publicar Postagens:\n5-Listar Postagens Publicadas:\n6-Listar Todas as Postagens\n7-Apagar Postagens\n8-voltar\n'))
+
+            if verificacao == 3:
+                
+
+                
+
 
             
 
