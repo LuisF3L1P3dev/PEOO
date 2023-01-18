85b9716446a704df7dc9bcd66f78c931fc9c7fcb
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2023-01-18 14:27:52 -0300

    add q2

---
+++ AtivUML1/q2UML.py
@@ -0,0 +1,31 @@
+class pedido():
+    def __init__(self) -> None:
+        pass
+
+
+    def valor_total(self):
+        pass
+
+    def adicionar_item(self, item):
+        pass
+
+    def obter_total(self):
+        pass
+
+class itemPedido():
+    def quantidade(self):
+        pass
+
+    def itemPedido(self, produto: int, quantidade: int):
+        pass
+
+class Produto():
+    def __init__(self, codigo, valor, descricao):
+        self.codigo = codigo
+        self.valor = valor
+        self.decricao = descricao
+
+    def Produto(codigo: int, valor: float, descricao: str):
+        pass
+
+
