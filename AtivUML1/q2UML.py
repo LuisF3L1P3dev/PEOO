b77c450a0d9c94ce27dc4fafe22509821e98619f
Luis Felipe <dantas.luis@escolar.ifrn.edu.br>
2023-01-18 23:21:17 -0300

    q2 uml
    resolver problema do obter total

--- AtivUML1/q2UML.py
+++ AtivUML1/q2UML.py
@@ -1,23 +1,26 @@
-class pedido():
-    def __init__(self) -> None:
-        pass
+class Pedido():
 
+    itensPedidos = []
 
-    def valor_total(self):
-        pass
+    def __init__(self, valor_total):
+        self.valor_total = valor_total
 
     def adicionar_item(self, item):
-        pass
+        self.itensPedidos.append(item)
+
+        return self.itensPedidos
 
     def obter_total(self):
-        pass
+        soma = 0
+        for i in self.itensPedidos:
+            soma += soma + i.valor
+        return soma
 
-class itemPedido():
-    def quantidade(self):
-        pass
-
-    def itemPedido(self, produto: int, quantidade: int):
-        pass
+    class itemPedido():
+        def __init__(self, quantidade, produto, pedido):
+            self.quantidade = quantidade
+            self.produto = produto
+            self.pedido = pedido
 
 class Produto():
     def __init__(self, codigo, valor, descricao):
@@ -25,7 +28,17 @@ class Produto():
         self.valor = valor
         self.decricao = descricao
 
-    def Produto(codigo: int, valor: float, descricao: str):
-        pass
 
+if __name__ == '__main__':
+    
+    produto1 = Produto('0104040400', 100, 'creme de barbear')
+    produto2 = Produto('fefefefffe', 50, 'gilete MAX')
+    produto3 = Produto('777777777', 2000, 'Cadeira')
 
+    pedido1 = Pedido(1000)
+
+    itemP1 = Pedido.itemPedido(2, produto1, pedido1)
+
+    pedido1.adicionar_item(itemP1)
+
+    print(pedido1.obter_total())
